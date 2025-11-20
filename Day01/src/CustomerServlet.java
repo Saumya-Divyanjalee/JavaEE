5f95d7ce8e137ade32703555a552f0a31b27f1e3
saumy <saumyadivyanjalee529@gmail.com>
2025-11-20 11:54:09 +0530

    completed second-day

---
+++ Day01/src/CustomerServlet.java
@@ -0,0 +1,41 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("customer");
+        PrintWriter out = resp.getWriter();
+        out.println("customer");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("put");
+        PrintWriter out = resp.getWriter();
+        out.println("put");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("delete");
+        PrintWriter out = resp.getWriter();
+        out.println("delete");
+    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("post");
+//        PrintWriter out = resp.getWriter();
+//        out.println("post");
+//    }
+
+
+}
